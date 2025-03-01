381b5fec10666a17a8da6120aaeefb540f75c69e
AlejoOsorio <112147774+AlejoOsorio@users.noreply.github.com>
2025-03-01 17:28:57 -0500

    Se actualizo la entidad User

--- src/main/java/co/edu/uniquindio/ingesis/domain/User.java
+++ src/main/java/co/edu/uniquindio/ingesis/domain/User.java
@@ -5,12 +5,21 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "user")
 @Getter
 @Setter
 public class User extends PanacheEntity {
 
+    private String nombre;
+    private String apellido;
     private String email;
+    private int semestre;
+    private String cedula;
+    private String telefono;
     private String password;
+    private String User;
+    private Date fechaNacimiento;
 }
